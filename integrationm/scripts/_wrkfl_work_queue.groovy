bd029e67d5190d64f19e6ce902c3483a9ca5707d
jbarata <joao.barata@cultofbits.com>
2023-12-27 19:56:23 +0000

    Implements a WorkQueues cache on launch wi script based on a query per specific data definition
    assumes max 200 WQs per definition

--- integrationm/scripts/_wrkfl_work_queue.groovy
+++ integrationm/scripts/_wrkfl_work_queue.groovy
@@ -1,30 +1,58 @@
 import groovy.transform.Field
+import com.google.common.cache.*
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+
 
 @Field DEFS_TO_IGNORE = [
         "Work Queues"
         , "Work Item"
 ];
 
+
+@Field static workQueuesCache = CacheBuilder.newBuilder()
+        .expireAfterWrite(5, TimeUnit.MINUTES)
+        .build();
+
+if (msg.product == "recordm" && msg.type == "Work Queues") {
+    log.info("Invalidating WorkQueues cache")
+    workQueuesCache.invalidateAll()
+}
+
+
 if (msg.product == "recordm" && !DEFS_TO_IGNORE.contains(msg.type)) {
 
     def query = "specific_data.raw:\"${msg.type}\" AND launch_condition:*"
+    def wqHits = getWorkQueueshits(query)
 
-    recordm.stream("Work Queues", query, { hit ->
+    wqHits.each { hit ->
         def launchCondition = hit.value('Launch Condition')
-        log.info("Launch content: ${launchCondition.split("\n")}")
+        log.debug("Launch content: ${launchCondition.split("\n")}")
 
-        if (evaluate(launchCondition)) {
-            def possibleStates = hit.value('Possible States')
-            log.info("Possible States: ${possibleStates}")
+            if (evaluate(launchCondition)) {
+                def possibleStates = hit.value('Possible States')
+                log.info("Launch condition true: ${launchCondition.split("\n")}")
+                log.info("Possible States: ${possibleStates}")
 
-            Map updates = [:]
-            updates["Customer Data"] = msg.instance.id
-            updates["Work Queue"] = hit.value("id")
-            updates["State"] = possibleStates[0]
+                Map updates = [:]
+                updates["Customer Data"] = msg.instance.id
+                updates["Work Queue"] = hit.value("id")
+                updates["State"] = possibleStates[0]
 
-            log.info("updates: ${updates}")
-            recordm.create("Work Item", updates)
-        }
-    })
+                log.info("updates: ${updates}")
+                recordm.create("Work Item", updates)
+            }
+    }
+}
 
+//Assumes max 200 WQs per query
+def getWorkQueueshits(query) {
+    try {
+        return workQueuesCache.get(
+                query,
+                { recordm.search("Work Queues", query, [size: 200]).getHits() }
+        )
+    } catch (ExecutionException ignore) {
+        return [];
+    }
 }
